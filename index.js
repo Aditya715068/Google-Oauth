68599751ff8854f8f6a0164715e7ce3704aed8e7
Aditya715068 <78248202+Aditya715068@users.noreply.github.com>
2021-09-06 10:23:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express  = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const PORT  = process.env.PORT || 5000
+require('./models/User')
+require('./services/passport')
+
+
+mongoose.connect(keys.mongoURI,{
+    useNewUrlParser:true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify:true
+
+}).then(()=>{
+    console.log("connection done");
+}).catch((err)=> console.log(`no connection`));
+  
+
+app.use(
+    cookieSession({
+        maxAge:30 * 24 * 60 * 60 *1000,
+        keys:[keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+require('./routes/authRoute')(app)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+
+app.listen(PORT,()=>{
+    console.log("server running on "+ PORT)
+})
