a1aeb1c01c3ddf76717f1de80b9c5b20b20caa73
Aditya715068 <78248202+Aditya715068@users.noreply.github.com>
2021-09-06 10:21:32 +0530

    Add files via upload

---
+++ clients/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = ()=>{
+    return (
+         <div className="card" style={{margin:"10%",padding:"20px",textAlign:"center"}}>
+             <div className="row">
+              <a href="/auth/google" className="waves-effect waves-light btn deep-purple darken-2">signup with google</a>   
+             </div>
+             
+            
+         </div>
+    )
+}
+
+
+export default Home;
